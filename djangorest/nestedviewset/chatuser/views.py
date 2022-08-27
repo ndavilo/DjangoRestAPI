224ca0ae47365a5f8160d6237b27ea72478e4799
Chukwunonso David Ilonze <nonsodavilo@gmail.com>
2022-08-27 09:08:04 +0100

    added filter to views

--- djangorest/nestedviewset/chatuser/views.py
+++ djangorest/nestedviewset/chatuser/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from .models import Chat, Person
 from .serializers import ChatSerializer, PersonSerializer
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ChatView(viewsets.ModelViewSet):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['sender', 'receiver']
 
 
 class PersonView(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['username', 'email']
 
