40dce958340d0cf5973659f0c4ff3a1de111ec6c
Chukwunonso David Ilonze <nonsodavilo@gmail.com>
2022-08-27 09:13:35 +0100

    added limitoffset

--- djangorest/nestedviewset/chatuser/views.py
+++ djangorest/nestedviewset/chatuser/views.py
@@ -3,10 +3,12 @@ from .models import Chat, Person
 from .serializers import ChatSerializer, PersonSerializer
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import LimitOffsetPagination
 
 class ChatView(viewsets.ModelViewSet):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+    pagination_class = LimitOffsetPagination
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['sender', 'receiver']
 
@@ -14,6 +16,7 @@ class ChatView(viewsets.ModelViewSet):
 class PersonView(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    pagination_class = LimitOffsetPagination
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['username', 'email']
 
